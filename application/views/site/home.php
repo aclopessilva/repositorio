7dbc70499c42aa6844defcd117ac6e8b959b94d2
Ana <ana.lopsil@gmail.com>
2017-04-28 23:01:34 -0300

    ajustando para funcionar localmente

--- application/views/site/home.php
+++ application/views/site/home.php
@@ -1,5 +1,5 @@
 <div class="text-center">
-	<img src="../images/Welcome-Banner.jpg" class="img-responsive">
+	<img src="<?php echo base_url('images/Welcome-Banner.jpg'); ?>" class="img-responsive">
 	<h1>Bem-vindo(a) ao meu site pessoal :)</h1>
 	<h2>Navegue atraves das opcoes do menu</h2>
 	<h3>Deixe seu comentario em 'Contato'</h3>
